46816bff65dd3bb7cc25b4f41a1ed4a264adc271
carletex <oceanrdn@gmail.com>
2015-04-05 18:13:52 +0200

    Better tests

--- II-Recursion/recursion_1.php
+++ II-Recursion/recursion_1.php
@@ -1,15 +1,9 @@
 <?php
-/*
+/**
  * Given an array of N integers, write a recursive function to
  * get the sum
  *
  */
-assert_options(ASSERT_BAIL, 1);
-
-$testArray = array(2,1,5,6,8);
-$testArray1 = array(0,1,0,3);
-$testArray2 = array(2,0,5,-1);
-
 function arraySum (array $numbers) {
   if (empty($numbers)) {
     return 0;
@@ -17,8 +11,23 @@ function arraySum (array $numbers) {
   return arraySum(array_slice($numbers, 1)) + $numbers[0];
 }
 
-assert(arraySum($testArray) == 22, "Array sum for testArray");
-assert(arraySum($testArray1) == 4, "Array sum for testArray1");
-assert(arraySum($testArray2) == 6, "Array sum for testArray2");
+/**
+ * Tests
+ */
+assert_options(ASSERT_BAIL, 1);
 
-print "Array Sum works\n";
+$tests = array(
+  22 => array(2,1,5,6,8),
+  4 => array(0,1,0,3),
+  -4 => array(2,0,-5,-1),
+);
+
+foreach ($tests as $expected => $testArray) {
+  $result = arraySum($testArray);
+  assert(
+    $result == $expected,
+    implode($testArray, ',') . ' expected ' . $expected . ' got ' . $result
+  );
+}
+
+print "All test passed. Array Sum works\n";
