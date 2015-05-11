6d7e3bee0a18732f6cd3b96c506d37dfc43e7027
carletex <oceanrdn@gmail.com>
2015-05-11 14:55:25 -0400

    Fundamentals - 1

---
+++ I-Fundamentals/bigO_1.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Determine the runtime and memory complexities of finding
+ * the average of an integer array.
+ */
+function average(array $numbers) {
+  $len = 0; // O(1)
+  $sum = 0; // O(1)
+  foreach ($numbers as $number) { // O(n)
+    $sum += $number; // O(1)
+    // We don't use count() to control
+    // what is really affecting.
+    $len++; // O(1)
+  }
+  return $sum / $len; // O(1)
+}
+
+// Runtime: O(n)
+// Memory (extra): O(1)
+
+/**
+ * Tests
+ */
+assert_options(ASSERT_BAIL, 1);
+
+$tests = array(
+  array('input' => array(1,2,3,4,5), 'expected' => 3),
+  array('input' => array(0,1,0,3), 'expected' => 1),
+);
+
+foreach ($tests as $test) {
+  $result = average($test['input']);
+  assert(
+    $result === $test['expected'],
+    implode($test['input'], ',') . ' expected ' . $test['expected'] . ' got ' . $result
+  );
+}
+
+print "All test passed. Averdage function works\n";
