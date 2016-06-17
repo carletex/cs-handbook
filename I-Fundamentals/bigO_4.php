4f696f3f36d682a700d435526a8a10af9a430c03
carletex <oceanrdn@gmail.com>
2016-06-17 08:19:35 +0200

    BigO_4 implementation

---
+++ I-Fundamentals/bigO_4.php
@@ -0,0 +1,31 @@
+<?php 
+
+/**
+ * Determine the runtime complexity of summing the digits of
+ * a number.
+ */
+
+function sumDigits($number) {
+	$sum = 0; // O(1)
+	do {
+		$sum += $number % 10; // O(1)
+	} while ($number = (int) $number / 10) // O(log n)
+}
+
+// Runtime: O(log n + 1 + 1) = O(log n)
+
+/**
+ * Tests
+ */
+assert_options(ASSERT_BAIL, 1);
+
+$number = 1234;
+$expected = 10;
+
+$result = sumDigits($number);
+assert(
+	$result === $expected,
+	'Expected ' . $expected . ' got ' . $result
+);
+
+print "Test passed. sumDigits function works\n";
