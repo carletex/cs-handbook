7d44943ae9289546c54a7e12f4f7fc35c7bcda46
carletex <oceanrdn@gmail.com>
2016-07-05 09:26:47 +0200

    Hash set implementation

--- III-Data_Structures/queue_linked_list.php
+++ III-Data_Structures/queue_linked_list.php
@@ -8,6 +8,10 @@ class Link {
 		$this->value = $value;
 		$this->next = NULL;
 	}
+
+	public function __toString() {
+		return (string)$this->value;
+	}
 }
 
 class LinkedList {
@@ -61,12 +65,45 @@ class LinkedList {
 		throw new Exception('Invalid index');
 	}
 
+	/**
+	 * Delete firts ocurrence of a node with $value
+	 */
+	public function deleteFirst($value) {
+		if ($this->head->value == $value) {
+			// The value is the first element
+			$this->head = $this->head->next;
+			$this->size--;
+			if ($this->size == 0) {
+				$this->end = NULL;
+			}
+			return TRUE;
+		}
+		for ($i = 0; $i < $this->size; $i++) { 
+			$node = $this->get($i);
+			if ($node->next->value == $value) {
+				$this->deleteNext($node);
+				return TRUE;
+			}
+		}
+		return FALSE;
+	}
+
 	public function deleteNext(Link $node) {
 		if ($node->next == $this->end) {
 			$this->end = $node;
 		}
 		$node->next = $node->next->next;
 		$this->size--;
+	}
+
+	public function contains($value) {
+		for ($i = 0; $i < $this->size; $i++) { 
+			$node = $this->get($i);
+			if ($node->value == $value) {
+				return TRUE;
+			}
+		}
+		return FALSE;
 	}
 
 }
