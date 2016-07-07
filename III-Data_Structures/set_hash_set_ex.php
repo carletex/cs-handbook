866911da9ee036b622903cc16c3829eaeec9f946
carletex <oceanrdn@gmail.com>
2016-07-07 08:08:53 +0200

    Data structures: Hash set ex3

--- III-Data_Structures/set_hash_set_ex.php
+++ III-Data_Structures/set_hash_set_ex.php
@@ -26,3 +26,35 @@ function getCollisionProbability($numBuckets, $numElements) {
 	// and the number of buckets
 	return (float) $numElements / $numBuckets;
 }
+
+/**
+ * 3. Prove that the runtime of all the operations are O(1).
+ */
+
+/*
+
+If the hash set is well distributed (the hash key are well generated/distributed)
+the runtime of all operations are O(1). We have to set a good collision ration so
+the hashSet resizes accordingly
+
+Membership
+	- Get linked list hash: 0(1)
+	- Look for the element in the linked list: O(1) because increment of elements
+	  in the hashSet the bucket numer increase accordingly (checking the collision ratio)
+	  so the runtime is constant. Not the case for memory Big O!!
+
+	Ex: 
+		Collision Ratio = 0.5 / Initial buckets = 10
+		10 Elements in hashSet => 20 buckets (10*2)
+		100 Elements in hashSet => 320 buckets (10*2*2*2...)
+		...
+
+Insert
+ 	- Chech membership: O(1)
+ 	- Push linkedlist: O(1) EndNode->next = newNode => The size of the linked list does
+ 	  not matter.
+Delete
+	- Remove form linkedList: O(1) As mentioned above, because of the resize of the 
+      hashSet buckets when the hashSet grows, the number os elements in the hashSet
+      does not matter.
+*/
