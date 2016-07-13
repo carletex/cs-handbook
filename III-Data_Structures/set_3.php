e6f816ee04f6eb6c423e20d888f75c6d60b69a7c
carletex <oceanrdn@gmail.com>
2016-07-13 08:19:19 +0200

    Data structures: Set ex3

---
+++ III-Data_Structures/set_3.php
@@ -0,0 +1,65 @@
+
+<?php 
+
+/**
+ * 3. Given an array of numbers, find the number of pairs of
+ * numbers that sum to 0.
+ */
+
+include_once './set_binary_search_tree.php';
+
+function getMutualFriends($f1, $f2) {
+    $tree = new BinarySearchTree();
+
+    // Populate tree
+    foreach ($f1 as $friend) {
+        $tree->insert($friend);
+    }
+    $initialTreeSize = $tree->size;
+
+    // Remove f2 from the tree
+    foreach ($f2 as $friend) {
+        $tree->remove($friend);
+    }
+
+    return $initialTreeSize - $tree->size;
+}
+
+/**
+ * Tests
+ */
+assert_options(ASSERT_BAIL, 1);
+
+$tests = array(
+    array(
+        'input' => array(
+            array('carlos', 'luis', 'pedro', 'cristina', 'laura'),
+            array('luis', 'carolina', 'patricia', 'pedro')
+        ), 
+        'expected' => 2
+    ),
+    array(
+        'input' => array(
+            array('carlos', 'luis', 'pedro', 'cristina', 'laura'),
+            array('luis', 'cristina', 'carlos', 'laura', 'pedro'),
+        ), 
+        'expected' => 5
+    ),
+    array(
+        'input' => array(
+            array('carlos', 'luis', 'pedro', 'cristina', 'laura'),
+            array('antonio', 'jim', 'victor'),
+        ), 
+        'expected' => 0
+    ),
+);
+
+foreach ($tests as $test) {
+    $result = getMutualFriends($test['input'][0], $test['input'][1]);
+    assert(
+        $result === $test['expected'],
+        json_encode($test['input']) . ' expected ' . $test['expected'] . ' got ' . $result
+    );
+}
+
+print "All test passed. getMutualFriends works\n";
